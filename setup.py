20d3795b082354a94b360921566a5666e97f8631
Thiago L. Pozati <thiagoleal11@gmail.com>
2023-08-26 11:14:33 -0300

    feat: Setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_models',
+    description='Custom models for deeplearning.',
+    version='0.0.1',
+    url='https://github.com/ThiagoLeal11/ml-models',
+    author='Thiago L. Pozati',
+    author_email='thiagoleal11@gmail.com',
+    keywords=['model', 'ml', 'machine', 'learning', 'deep', 'deeplearning'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'torch',
+    ],
+)
