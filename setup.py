8d9cce0428c432091bbb4bc17ab736ead4606c47
Thiago L. Pozati <thiagoleal11@gmail.com>
2023-08-27 22:44:24 -0300

    Fix: importing issues

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ml_models',
     description='Custom models for deeplearning.',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/ThiagoLeal11/ml-models',
     author='Thiago L. Pozati',
     author_email='thiagoleal11@gmail.com',
